db94372819f4018f3508de784f1fa2df713321a2
AJWest22 <abiwestcott@gmail.com>
2021-10-05 15:15:21 +0000

    added the fantasy accordion

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_genres")
 def get_genres():
-    books = mongo.db.books.find()
+    books = list(mongo.db.books.find())
     return render_template("books.html", books=books)
 
 
